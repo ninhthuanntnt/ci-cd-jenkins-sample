c16be6f4badd3e8d8a699a45d5606b604f36710f
ninhthuanntnt <ninhthuan1999@gmail.com>
2023-09-10 23:33:50 +0700

    Add example service and modify Jenkinsfile for sonar

---
+++ example/src/main/java/com/ntnt/example/ExampleService.java
@@ -0,0 +1,10 @@
+package com.ntnt.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleService {
+  public void print() {
+    System.out.println("ExampleService");
+  }
+}
